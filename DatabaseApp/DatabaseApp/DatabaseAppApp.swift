3de523eb93da513e9b34b11b06c04f89ec0198cd
Pablo Lasarte <pl8n4@umsystem.edu>
2024-04-18 18:55:55 -0500

    Updated app launch

---
+++ DatabaseApp/DatabaseApp/DatabaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseAppApp.swift
+//  DatabaseApp
+//
+//  Created by Pablo Lasarte on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct DatabaseAppApp: App {
+        var body: some Scene {
+            WindowGroup {
+                WelcomePage()
+            }
+        }
+}
